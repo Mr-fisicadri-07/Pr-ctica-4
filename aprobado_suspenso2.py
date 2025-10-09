cc2b413b50be702f914b621104692d93421f1639
Mr-fisicadri-07 <adrianlof07@gmail.com>
2025-10-09 08:20:35 +0200

    Created pass-fail program
    I've creared a program to determine whether a student passes or fails a subject

---
+++ aprobado_suspenso2.py
@@ -0,0 +1,13 @@
+# this program decides whether a student has passed or failed based on their grades and attendance.
+
+exam_score = int(input("Enter the exam score (0-100): "))
+
+attended_classes = int(input("Enter the number of classes attended (0-40): "))
+
+attendance_percentage = (attended_classes /40) * 100
+
+if exam_score >= 70 and attendance_percentage >= 80:
+    print(f"The student has passed with a {exam_score}.")
+else:
+    print(f"The student has failed with a {exam_score}.")
+
